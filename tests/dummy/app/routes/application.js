5848ce3de14f83f45984bd868fe0e8b7b37c56f9
snewcomer <snewcomer24@gmail.com>
2021-02-15 12:46:01 -0600

    get some tests passing

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,13 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
-  model() {
-    return import('d3')
-      .then((d3) => {
-        return d3.version;
-      })
-      .catch((error) => {
-        return 'Error: ' + error;
-      });
-  }
 }
